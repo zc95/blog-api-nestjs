f5634ab2f2943e600a42b1b80b77a84d21e70ceb
zhangcheng <571346800@qq.com>
2022-01-09 12:31:07 +0800

    feature: 添加swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setSwagger } from './utils/swagger.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger
+  setSwagger(app);
+
   await app.listen(3000);
 }
+
 bootstrap();
